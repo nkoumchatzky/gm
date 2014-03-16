977853f14883c1cfb0bdb4da12d8573e5054e095
nkoumchatzky <nicolas.koumchatzky@gmail.com>
2014-03-16 22:09:12 +0100

    * Add solar system training

--- examples.lua
+++ examples.lua
@@ -101,20 +101,10 @@ function gm.examples.simple()
    --print(edgeBel)
    print('<gm.testme> log(Z):')
    print(logZ)
+--   local samples = g:sample('exact',5)
+--   print('samples = ', samples)
 end
 
-----------------------------------------------------------------------
--- Example of how to train an SolarSystem with suns and satellites
---
-function gm.examples.trainSolar()
-   nSuns = 10
-   nSatellites = 10
-   nStates = 2
-   adjacency = gm.adjacency.solarSystem(nSuns, nSatellites)
-   -- TODO : set type to 'solar' 
-   g = gm.undirectedGraph{adjacency=adjacency, nStates=nStates, maxIter=10, type='generic', verbose=true}
-   return g   
-end
 ----------------------------------------------------------------------
 -- Example of how to train an MRF
 --
@@ -159,10 +149,18 @@ function gm.examples.trainMRF()
    -- pairwise relations globally
    nEdges = g.edgeEnds:size(1)
    edgeMap = zeros(nEdges,nStates,nStates)
+   
+--   for i=1, nEdges do
+--      edgeMap[{ i,1,1 }] = nNodes+i
+--      edgeMap[{ i,2,2 }] = nNodes+i+1
+--      edgeMap[{ i,1,2 }] = nNodes+i+2
+--   end
    edgeMap[{ {},1,1 }] = nNodes+1
    edgeMap[{ {},2,2 }] = nNodes+2
    edgeMap[{ {},1,2 }] = nNodes+3
 
+   print("MRF nodeMap = ", nodeMap)
+   print("MRF edgeMap = ", edgeMap)
    -- initialize parameters
    g:initParameters(nodeMap,edgeMap)
    
@@ -202,6 +200,174 @@ function gm.examples.trainMRF()
    print(samples)
 end
 
+
+----------------------------------------------------------------------
+-- Example of how to train an SolarSystem with suns and satellites
+--
+function gm.examples.trainSolarSystem()
+   nSuns = 2
+   nSatellites = 4--nSuns * nSuns
+   nSunsStates = 2
+   nSatellitesStates = 2
+   
+   
+   -- define fully connected sub-graph
+   subAdjacency = gm.adjacency.full(nSuns)
+   subGraph = gm.undirectedGraph{adjacency=subAdjacency, nStates=nSunsStates, maxIter=10, verbose=true}
+
+   -- unary potentials
+   nodePot = tensor{{1,1}, {1,1}}
+   print ('nodePot = ', nodePot)
+   -- joint potentials
+   edgePot = tensor(subGraph.nEdges,nSunsStates,nSunsStates)
+   basic = tensor{{1,1}, {1,1}}
+   for e = 1,subGraph.nEdges do
+      edgePot[e] = basic
+   end
+
+   -- set potentials
+   subGraph:setPotentials(nodePot,edgePot)
+
+   
+   -- The satellite has value 1 or 2 (e.g. not happy or happy for a user)
+   nbStates = nSatellitesStates
+   -- All the satellites have an equal number of instances 
+   
+   nNodes = nSatellites
+   -- build mrf adjacency matrix because nodes HAVE TO have edges... 
+   -- TODO : change that annoying fact
+   adjacency = torch.zeros(nNodes,nNodes)
+   for i = 1,nNodes-1 do
+      adjacency[i][i+1] = 1
+      adjacency[i+1][i] = 1
+   end
+   -- TODO : set type to 'solar' 
+   g = gm.undirectedGraph{adjacency=adjacency, nStates=nbStates, maxIter=10, type='crf', verbose=true}
+   
+   -- make labels for MAP (list of labels)
+   -- nInstances/nSatellites first pictures belong to satellite 1, nInstances/nSatellites next to satellite 2, ...
+   -- user 1 likes only the nInstances/nSatellites first classes (suns), user 2 likes only the rest  
+   nInstances = 100000
+   
+   
+   -- sampling from the sub graph
+   X = subGraph:sample('exact',nInstances)
+   local subNodeBel,subEdgeBel,subLogZ = subGraph:infer('bp')
+   print('<gm.testme> subnode beliefs:')
+   print(subNodeBel)
+   
+--   X = X-1
+   
+   
+   -- labels : user 1 happy as soon as first node of sub-graph is activated, user 2 the same wrt the second node 
+   print('samples = ', samples)
+   y = tensor(nInstances,nSatellites)
+   nNodeFeatures = 3
+   
+   -- create node features (normalized X and a bias)
+   -- create edge features
+   Xnode = tensor(nInstances,nNodeFeatures,nNodes)
+   nodeMap = zeros(nNodes, nbStates, nNodeFeatures)
+   nEdges = g.edgeEnds:size(1)
+   nEdgeFeatures = 1 -- no edges features
+   Xedge = zeros(nInstances,nEdgeFeatures,nEdges)
+   edgeMap = zeros(nEdges,nbStates,nbStates,nEdgeFeatures)
+   local X1 = X[{ {},1 }]
+   local X2 = X[{ {},2 }]
+   local m1 = X1:mean()
+   local m2 = X2:mean()
+   
+   for i=1,nInstances do 
+      if X[i][1] == 1 then
+         y[i][1] = 1
+      end 
+      if X[i][1] == 2 then
+         y[i][2] = 1
+      end
+      if X[i][1] == 1 and X[i][2] == 1 then
+         y[i][3] = 1
+      end
+      if X[i][1] == 2 and X[i][2] == 2 then
+         y[i][4] = 1
+      end
+      
+      Xnode[{i, 1, {}}] = X[i][1]-m1
+      Xnode[{i, 2, {}}] = X[i][2]-m2
+      Xnode[{i, 3, {}}] = (X[i][1]-m1)*(X[i][2]-m2)
+   end
+   y = y+1
+   Xnode = Xnode
+   
+   counter = 1
+   -- Different parameters for every node and every feature.
+   for i=1, nNodes do
+      for f=1,nNodeFeatures do
+         nodeMap[i][2][f] = counter
+--         nodeMap[i][2][f] = counter+1
+         counter = counter + 1
+      end
+   end
+   
+  
+   -- initialize parameters
+   g:initParameters(nodeMap,edgeMap)
+   -- and train
+   require 'optim'
+   local sgdState = {
+      learningRate = 1e-3,
+      learningRateDecay = 1e-2,
+      weightDecay = 1e-5,
+   }
+   for iter = 1,10000 do
+      -- SGD step:
+      optim.sgd(function()
+         -- random sample:
+         local i = torch.random(1,nInstances)
+         -- compute f+grad:
+         local f,grad = g:nll('bp',y[i],Xnode[i],Xedge[i])
+         -- verbose:
+         print('SGD @ iteration ' .. iter .. ': objective = ', f)
+         -- return f+grad:
+         return f,grad
+      end, 
+      g.w, sgdState)
+   end
+
+   print('g.w = ', g.w)
+   local nodeBel,edgeBel,logZ = g:infer('bp',1,1)
+   print('<gm.testme> node beliefs:')
+   print(nodeBel)
+   print('<gm.testme> edge beliefs:')
+   print(edgeBel)
+   print('<gm.testme> logZ:')
+   print(logZ)
+   
+   testX = tensor(nSuns)
+   testNode = tensor(nNodeFeatures, nNodes)
+   testX[1] = 1
+   testX[2] = 2
+   
+   testNode[{1, {}}] = testX[1]-m1
+   testNode[{2, {}}] = testX[2]-m2
+   testNode[{3, {}}] = (testX[1]-m1)*(testX[2]-m2)
+
+   testEdge = Xedge[1] --whatever...
+   g:makePotentials(testNode,testEdge)
+   local nodeBel,edgeBel,logZ = g:infer('exact',1,1)
+   print('<gm.testme 2> node beliefs:')
+   print(nodeBel)
+   print('<gm.testme 2> edge beliefs:')
+   print(edgeBel)
+   print('<gm.testme 2> logZ:')
+   print(logZ)
+   labeling = g:decode('exact')
+   
+   print('<gm.testme 2> labeling:')
+   print(labeling)
+--   os.exit()
+   return g   
+end
+
 ----------------------------------------------------------------------
 -- Example of how to train a CRF for a simple segmentation task
 --
@@ -212,7 +378,6 @@ function gm.examples.trainCRF()
    nNodes = nRows*nCols
    nStates = 2
    nInstances = 100
-
    -- make labels (MAP):
    y = tensor(nInstances,nRows*nCols)
    for i = 1,nInstances do
@@ -227,6 +392,9 @@ function gm.examples.trainCRF()
    end
    X = X + randn(X:size())/2
 
+   print('y[1] = ', y[1])
+   print('X[1] = ', X[1])
+--   os.exit()
    -- display a couple of input examples
    require 'gfx.js'
    gfx.image({
@@ -239,7 +407,7 @@ function gm.examples.trainCRF()
    })
 
    -- define adjacency matrix (4-connexity lattice)
-   local adj = gm.adjacency.lattice2d(nRows,nCols,8)
+   local adj = gm.adjacency.lattice2d(nRows,nCols,4)
 
    -- create graph
    g = gm.undirectedGraph{adjacency=adj, nStates=nStates, verbose=true, type='crf', maxIter=10}
@@ -259,7 +427,8 @@ function gm.examples.trainCRF()
    end
    Xnode[{ {},2 }] = X -- features (simple normalized grayscale)
    nNodeFeatures = Xnode:size(2)
-
+   print('Xnode = ', Xnode)
+   print('nNodeFeatures = ', nNodeFeatures)
    -- tie node potentials to parameter vector
    nodeMap = zeros(nNodes,nStates,nNodeFeatures)
    for f = 1,nNodeFeatures do
@@ -285,6 +454,7 @@ function gm.examples.trainCRF()
       end
    end
 
+   print('Xedge = ', Xedge)
    -- tie edge potentials to parameter vector
    local f = nodeMap:max()
    edgeMap = zeros(nEdges,nStates,nStates,nEdgeFeatures)
@@ -318,6 +488,8 @@ function gm.examples.trainCRF()
       g.w, sgdState)
    end
 
+   print('g.w = ', g.w)
+   os.exit()
    -- the model is trained, generate node/edge potentials, and test
    marginals = {}
    labelings = {}
